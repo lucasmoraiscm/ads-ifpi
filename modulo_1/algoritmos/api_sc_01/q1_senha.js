f1dd8d81f9e80bc2a40dee310570e4b05e688114
Lucas Morais <108703672+lucasmoraiscm@users.noreply.github.com>
2024-04-22 13:38:58 -0300

    Add files via upload

---
+++ modulo_1/algoritmos/api_sc_01/q1_senha.js
@@ -0,0 +1,68 @@
+import { show } from '../utils/io_utils.js'
+import { get_positive_number } from '../utils/math_utils.js'
+
+function main(){
+    let status = 1
+
+    while (status != 0){
+        const digitos = get_positive_number('Quantidade de dígitos: ')
+
+        const senha = gerar_senha(digitos)
+
+        show(`Senha sugerida: ${senha}`)
+
+        show(`
+1 - Sim 
+2 - Não`)
+        const pergunta = get_positive_number('Você está satisfeito com essa senha?: ')
+
+        if (pergunta == 1) {
+            status --
+        }
+    }
+}
+
+function gerar_senha(d){
+    let senha = ''
+    let n = gerar_n_aleatorio()
+    let n_anterior = n
+    senha += n
+    let counter = 1
+
+    while (counter < d){
+        n = gerar_n_aleatorio()
+
+        if (n != n_anterior){
+            if (n > 0){
+                if (n < 9){
+                    if (n_anterior != n - 1 && n_anterior != n + 1){
+                        senha += n
+                        n_anterior = n
+                        counter ++
+                    }
+                }else{
+                    if (n_anterior != 8){
+                        senha += n
+                        n_anterior = n
+                        counter ++
+                    }
+                }
+            }else{
+                if (n_anterior != 1){
+                    senha += n
+                    n_anterior = n
+                    counter ++
+                }
+            }
+        }
+    }
+
+    return senha
+}
+
+function gerar_n_aleatorio(){
+    const n = Math.floor(Math.random() * 9)
+    return n
+}
+
+main()
---
+++ modulo_1/algoritmos/api_sc_01/q2_escada_transport.js
@@ -0,0 +1,54 @@
+import { show } from '../utils/io_utils.js'
+import { get_positive_number } from '../utils/math_utils.js'
+
+function main(){
+    const peso_atual = get_positive_number('Peso atual: ')
+    const sexo = get_positive_number('Sexo (1-Masculino ou 2-Feminino): ')
+    const quilos_a_perder = get_positive_number('Quantos quilos deseja perder?: ')
+    const dias_atividade = get_positive_number('Quantos dias por semana realizará atividade física?: ')
+    const tempo_dia = get_positive_number('Quantos minutos irá treinar por dia?: ')
+    const proporcao_transport = get_positive_number('Qual a proporção de tempo alocada para o Transport?: ')
+
+    const calorias_a_perder = 7000 * quilos_a_perder
+    const tempo_transport = tempo_dia * (proporcao_transport / 100)
+    const tempo_escada = tempo_dia - tempo_transport
+    let gasto_calorias_transport = calcular_gasto_calorias_transport(tempo_transport, dias_atividade, sexo)
+    let gasto_calorias_escada = calcular_gasto_calorias_escada(tempo_escada, dias_atividade, sexo)
+    let gasto_calorias_total = 0
+    let semanas = 0
+
+    while (calorias_a_perder > gasto_calorias_total){
+        semanas ++
+        gasto_calorias_total = (gasto_calorias_transport * semanas) + (gasto_calorias_escada * semanas)
+    }
+
+    const relatorio = `
+Quantidade de semanas para alcançar o objetivo desejado: ${semanas} semanas
+Tempo de Transport por dia: ${Math.floor(tempo_transport)} min
+Tempo de Escada por dia: ${Math.floor(tempo_escada)} min`
+    show(relatorio)
+}
+
+function calcular_gasto_calorias_transport(tempo, dias, sexo){
+    let calorias = 0
+    if (sexo == 1){
+        calorias = ((tempo / 5) * 100) * dias
+    }else{
+        calorias = ((tempo / 6) * 100) * dias
+    }
+    
+    return calorias
+}
+
+function calcular_gasto_calorias_escada(tempo, dias, sexo){
+    let calorias = 0
+    if (sexo == 1){
+        calorias = ((tempo / 7) * 100) * dias
+    }else{
+        calorias = ((tempo / 8) * 100) * dias
+    }
+    
+    return calorias
+}
+
+main()
