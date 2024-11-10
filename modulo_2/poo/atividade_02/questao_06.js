02bdf4892b69900125c91aea6f0446233087b6a8
Lucas Morais <108703672+lucasmoraiscm@users.noreply.github.com>
2024-11-09 21:10:19 -0300

    Add files via upload

---
+++ modulo_2/poo/atividade_02/questao_06.js
@@ -0,0 +1,7 @@
+function main() {
+    var nome = "Ely";
+    var time = 120.56;
+    var language = "TypeScript";
+    console.log("".concat(nome, "\nMy payment time is ").concat(time, "\nand\nmy preffered language is ").concat(language));
+}
+main();
---
+++ modulo_2/poo/atividade_02/questao_06.ts
@@ -0,0 +1,8 @@
+let nome : string = "Ely";
+let time : number = 120.56;
+let language : string = "TypeScript";
+
+console.log(`${nome}
+My payment time is ${time}
+and
+my preffered language is ${language}`)
