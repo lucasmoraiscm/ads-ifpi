762738df5b27627616605c51625f3cb1c89d5e7c
Lucas Morais <108703672+lucasmoraiscm@users.noreply.github.com>
2024-11-09 21:13:03 -0300

    Delete modulo_2/poo/atividade_02 directory

--- modulo_2/poo/atividade_02/questao_06.ts
+++
@@ -1,8 +0,0 @@
-let nome : string = "Ely";
-let time : number = 120.56;
-let language : string = "TypeScript";
-
-console.log(`${nome}
-My payment time is ${time}
-and
-my preffered language is ${language}`)
