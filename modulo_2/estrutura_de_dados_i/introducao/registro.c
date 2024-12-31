c69153c00e28ba1bf0bf4919b3e6b11a87e28876
Lucas Morais <108703672+lucasmoraiscm@users.noreply.github.com>
2024-12-31 14:45:06 -0300

    Delete modulo_2/estrutura_de_dados_i/introducao directory

--- modulo_2/estrutura_de_dados_i/introducao/registro.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-typedef struct {
-    int a; char b; float c;
-} reg;
-
-main(){
-    reg r = {18, 'a', 2.5};
-}
--- modulo_2/estrutura_de_dados_i/introducao/teste.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    int n;
-    printf("Digite um número: \n");
-    scanf("%d", &n);
-    printf("Numero: %d", n);
-    return 0;
-}
