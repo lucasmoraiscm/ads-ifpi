ee6dad01e9d6c541ef966282c67dda84f494cf33
Lucas Morais <108703672+lucasmoraiscm@users.noreply.github.com>
2025-07-03 21:22:17 -0300

    :recycle: refactor: Modifica estrutura do balanceamento com ordenação

--- modulo_3/estrutura_de_dados_ii/balanceamento_arvores/balanceamento_bubble_sort.c
+++ modulo_3/estrutura_de_dados_ii/balanceamento_arvores/balanceamento_bubble_sort.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
 
-typedef char* ITEM;
+typedef int ITEM;
 
 // Definindo a estrutura da árvore binária
 typedef struct arvore
@@ -23,12 +21,26 @@ Arv criar_no_arv(Arv no_esq, ITEM valor, Arv no_dir)
     return no;
 }
 
+Arv inserir(Arv arvore, ITEM valor)
+{
+    if (arvore == NULL) return criar_no_arv(NULL, valor, NULL);
+
+    if (valor < arvore->item) 
+    {
+        arvore->esq = inserir(arvore->esq, valor);
+    }
+    else if (valor > arvore->item)
+    {
+        arvore->dir = inserir(arvore->dir, valor);
+    }
+    
+    return arvore;
+}
+
 void bubble_sort(int quant, int* vetor) 
 {
-    int i, j;
-
-    for (i = quant - 1; i >= 1; i--) {
-        for (j = 0; j < i; j++) {
+    for (int i = quant - 1; i >= 1; i--) {
+        for (int j = 0; j < i; j++) {
             if (vetor[j] > vetor[j + 1]) {
                 int temp = vetor[j];
                 vetor[j] = vetor[j + 1];
@@ -38,23 +50,37 @@ void bubble_sort(int quant, int* vetor)
     }
 }
 
-void balancear(int* vetor, int first, int last) {
-    if (last <= first) {
-        int middle = (first + last) / 2;
-        //inserir(vetor[middle]); Fazer uma função inserir
-        balancear(vetor, first, middle - 1);
-        balancear(vetor, middle + 1, last);
+Arv balancear(Arv arvore, int* vetor, int first, int last) {
+    if (first > last) return arvore;
+
+    int middle = (first + last) / 2;
+    arvore = inserir(arvore, vetor[middle]);
+    arvore = balancear(arvore, vetor, first, middle - 1);
+    arvore = balancear(arvore, vetor, middle + 1, last);
+    return arvore;
+}
+
+void em_ordem(Arv arvore)
+{
+    if (arvore != NULL) 
+    {
+        em_ordem(arvore->esq);
+        printf("%d ", arvore->item);
+        em_ordem(arvore->dir);
     }
 }
 
 int main() 
-{
+{   
+    Arv arvore = NULL;
     int valores[] = {3, 9, 4, 7, 1, 6, 2, 5, 8};
     int quant = sizeof(valores) / sizeof(valores[0]);
 
-    bubble_sort(quant, &valores);
+    bubble_sort(quant, valores);
 
-    //balancear(&valores, valores[0], valores[9]);
+    arvore = balancear(arvore, valores, 0, quant - 1);
 
+    printf("\n");
+    em_ordem(arvore);
     return 0;
 }
